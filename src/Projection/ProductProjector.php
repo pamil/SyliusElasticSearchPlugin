62adb702f8070a76c84351d0eff55f60abdffd1f
Arvids Godjuks <arvids.godjuks@gmail.com>
2017-08-29 13:23:52 +0300

    Added product update and delete handlers to Doctrine listener

--- src/Projection/ProductProjector.php
+++ src/Projection/ProductProjector.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace Sylius\ElasticSearchPlugin\Projection;
 
+use ONGR\ElasticsearchBundle\Result\DocumentIterator;
 use ONGR\ElasticsearchBundle\Service\Manager;
+use ONGR\ElasticsearchBundle\Service\Repository;
 use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Locale\Model\LocaleInterface;
+use Sylius\ElasticSearchPlugin\Document\ProductDocument;
 use Sylius\ElasticSearchPlugin\Event\ProductCreated;
+use Sylius\ElasticSearchPlugin\Event\ProductDeleted;
+use Sylius\ElasticSearchPlugin\Event\ProductUpdated;
 use Sylius\ElasticSearchPlugin\Factory\ProductDocumentFactoryInterface;
 
 final class ProductProjector
@@ -22,6 +29,11 @@ final class ProductProjector
     private $productDocumentFactory;
 
     /**
+     * @var Repository
+     */
+    private $productDocumentRepository;
+
+    /**
      * @param Manager $manager
      * @param ProductDocumentFactoryInterface $productDocumentFactory
      */
@@ -29,12 +41,13 @@ final class ProductProjector
     {
         $this->manager = $manager;
         $this->productDocumentFactory = $productDocumentFactory;
+        $this->productDocumentRepository = $this->manager->getRepository(ProductDocument::class);;
     }
 
     /**
      * @param ProductCreated $event
      */
-    public function handleProductCreated(ProductCreated $event)
+    public function handleProductCreated(ProductCreated $event): void
     {
         $product = $event->product();
         $channels = $product->getChannels();
@@ -54,5 +67,63 @@ final class ProductProjector
         }
 
         $this->manager->commit();
+    }
+
+    /**
+     * We create a new product documents with updated data and remove old once
+     *
+     * @param ProductUpdated $event
+     */
+    public function handleProductUpdated(ProductUpdated $event): void
+    {
+        $product = $event->product();
+
+        $this->scheduleCreatingNewProductDocument($product);
+        $this->scheduleRemovingOldProductDocuments($product);
+
+        $this->manager->commit();
+    }
+
+    /**
+     * We remove deleted product
+     *
+     * @param ProductDeleted $event
+     */
+    public function handleProductDeleted(ProductDeleted $event): void
+    {
+        $product = $event->product();
+
+        $this->scheduleRemovingOldProductDocuments($product);
+
+        $this->manager->commit();
+    }
+
+    protected function scheduleCreatingNewProductDocument(ProductInterface $product): void
+    {
+        /** @var ChannelInterface[] $channels */
+        $channels = $product->getChannels();
+
+        foreach ($channels as $channel) {
+            /** @var LocaleInterface[] $locales */
+            $locales = $channel->getLocales();
+
+            foreach ($locales as $locale) {
+                $this->manager->persist($this->productDocumentFactory->createFromSyliusProductModel(
+                    $product,
+                    $locale,
+                    $channel
+                ));
+            }
+        }
+    }
+
+    protected function scheduleRemovingOldProductDocuments(ProductInterface $product): void
+    {
+        /** @var DocumentIterator|ProductDocument[] $currentProductDocuments */
+        $currentProductDocuments = $this->productDocumentRepository->findBy(['id' => $product->getId()]);
+
+        foreach ($currentProductDocuments as $sameCodeProductDocument) {
+            $this->manager->remove($sameCodeProductDocument);
+        }
     }
 }
