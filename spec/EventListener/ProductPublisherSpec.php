086b803db663343ee90cd6fcc13be09ad163e009
Kamil Kokot <kamil@kokot.me>
2017-09-29 14:34:37 +0200

    ProductPublisher: product updated when product variant image updated

--- spec/EventListener/ProductPublisherSpec.php
+++ spec/EventListener/ProductPublisherSpec.php
@@ -208,6 +208,26 @@ final class ProductPublisherSpec extends ObjectBehavior
         $this->postFlush($postFlushEvent);
     }
 
+    function it_publishes_product_updated_event_when_product_variant_image_is_updated(
+        MessageBus $eventBus,
+        OnFlushEventArgs $onFlushEvent,
+        PostFlushEventArgs $postFlushEvent,
+        UnitOfWork $unitOfWork,
+        ProductImageInterface $productImage,
+        ProductVariantInterface $productVariant,
+        ProductInterface $product
+    ): void {
+        $unitOfWork->getScheduledEntityUpdates()->willReturn([$productImage]);
+
+        $productImage->getOwner()->willReturn($productVariant);
+        $productVariant->getProduct()->willReturn($product);
+
+        $eventBus->handle(ProductUpdated::occur($product->getWrappedObject()))->shouldBeCalled();
+
+        $this->onFlush($onFlushEvent);
+        $this->postFlush($postFlushEvent);
+    }
+
     function it_publishes_product_deleted_event_when_product_is_deleted(
         MessageBus $eventBus,
         OnFlushEventArgs $onFlushEvent,
